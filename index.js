d4a0fda5e0dbcfbe0c2765b509c26a81cdabf87c
Garrett Lewis <glewis@conversantmedia.com>
2017-11-24 17:52:55 -0700

    fixed env

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ director    = require('director');
 cool        = require('cool-ascii-faces');
 bot         = require('./bot.js');
 
+var dotenv = require('dotenv');
+dotenv.load();
+
 router = new director.http.Router({
   '/' : {
     post: bot.respond,
@@ -28,13 +31,14 @@ port = Number(process.env.PORT || 5000);
 server.listen(port);
 
 function ping() {
+  console.log(process.env);
   var ye = '@garrettbot #jay cutler'
   var regex = /@garrettbot #[a-zA-Z ]+/
   var retval = '';
   if (regex.test(ye)) {
     retval = ye.substring(13).trim();
     this.res.writeHead(200);
-    this.res.end(retval);
+    this.res.end(JSON.stringify(process.env));
   
   }
   else {
