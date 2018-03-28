b436b9d8c0638a4bb305818951e2b105e247c793
Garrett Lewis <glewis@conversantmedia.com>
2018-03-28 15:53:29 -0500

    added eslint rules

--- bot.js
+++ bot.js
@@ -1,275 +1,253 @@
 
-var HTTPS = require('https');
-var nodeRequest = require('request');
-var cool = require('cool-ascii-faces');
-var fs = require('fs');
-var jokes = require('./jokes');
+const HTTPS = require('https');
+const nodeRequest = require('request');
+const cool = require('cool-ascii-faces');
+const fs = require('fs');
+const jokes = require('./jokes');
 const predict = require('eightball');
 
-var botID = process.env.BOT_ID;
-var _ = require('lodash');
+const botID = process.env.BOT_ID;
+const _ = require('lodash');
 
-var options = {
-  hostname: 'api.groupme.com',
-  path: '/v3/bots/post',
-  method: 'POST'
+const options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
 };
 
-var members = {};
-var apiKey = process.env.API_KEY;
+const members = {};
+const apiKey = process.env.API_KEY;
 
-var messages = [];
+const messages = [];
 
 
 function respond() {
-  const request = JSON.parse(this.req.chunks[0]),
-  // var request = {text: '-leaderboard'},  
-      yeRegex = /^Ye\?|ye\?$/,
+    const request = JSON.parse(this.req.chunks[0]),
+        // var request = {text: '-leaderboard'},
+        yeRegex = /^Ye\?|ye\?$/,
 	  shadesRegex = /((50|[fF]ifty) [sS]hades [Oo]f [Gg]r[ea]y)/,
-      gifRegex = /#[0-9a-zA-Z ]+/,
-      leaderRegex = /-leaderboard/;
-      directionsRegex = /[dD]irections from[:]? ([0-9a-zA-Z .,]+) [tT]o[:]? ([0-9a-zA-Z .,]+)/;
-      jokeRegex = /@?[gG]((arrett)|(urt))[bB]ot,? tell me a joke/;
-      eightBallRegex = /@?[gG]((arrett)|(urt))[bB]ot,? [a-zA-Z0-9 ]+\?{1}/;
-      urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)/
-  console.log(request.text);
-  if (request.text && urlRegex.test(request.text)) {
-    console.log("don't care");
-    this.res.writeHead(200);
-    this.res.end();
-  }
-  else if (request.text && yeRegex.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage();
-    this.res.end();
-  }
-  else if (request.text && shadesRegex.test(request.text)) {
+        gifRegex = /#[0-9a-zA-Z ]+/,
+        leaderRegex = /-leaderboard/;
+    directionsRegex = /[dD]irections from[:]? ([0-9a-zA-Z .,]+) [tT]o[:]? ([0-9a-zA-Z .,]+)/;
+    jokeRegex = /@?[gG]((arrett)|(urt))[bB]ot,? tell me a joke/;
+    eightBallRegex = /@?[gG]((arrett)|(urt))[bB]ot,? [a-zA-Z0-9 ]+\?{1}/;
+    urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)/;
+    console.log(request.text);
+    if (request.text && urlRegex.test(request.text)) {
+        console.log("don't care");
+        this.res.writeHead(200);
+        this.res.end();
+    } else if (request.text && yeRegex.test(request.text)) {
+        this.res.writeHead(200);
+        postMessage();
+        this.res.end();
+    } else if (request.text && shadesRegex.test(request.text)) {
 	  this.res.writeHead(200);
 	  gifTag('hot garbage');
 	  this.res.end();
-  }
-  else if (request.text && gifRegex.test(request.text)) {
-    this.res.writeHead(200);
-    console.log('gif requested');
-    gifTag(request.text);
-    this.res.end();
-  }
-  else if (request.text && jokeRegex.test(request.text)) {
-    this.res.writeHead(200);
-    console.log('telling a joke');
-    tellJoke();
-    this.res.end();
-  }
-  else if (request.text && eightBallRegex.test(request.text)) {
-    this.res.writeHead(200);
-    console.log('eight ball');
-    sendEightBallMsg();
-    this.res.end();
-  }
-  else if (request.text && leaderRegex.test(request.text)) {
-    this.res.writeHead(200);
-    doLeaderboard();
-    this.res.end();
-  }
-  else if (request.text && directionsRegex.test(request.text)) {
-    this.res.writeHead(200);
-    getDirections(request.text);
-    this.res.end();
-  }
-  else {
-    console.log("don't care");
-    this.res.writeHead(200);
-    this.res.end();
-  }
+    } else if (request.text && gifRegex.test(request.text)) {
+        this.res.writeHead(200);
+        console.log('gif requested');
+        gifTag(request.text);
+        this.res.end();
+    } else if (request.text && jokeRegex.test(request.text)) {
+        this.res.writeHead(200);
+        console.log('telling a joke');
+        tellJoke();
+        this.res.end();
+    } else if (request.text && eightBallRegex.test(request.text)) {
+        this.res.writeHead(200);
+        console.log('eight ball');
+        sendEightBallMsg();
+        this.res.end();
+    } else if (request.text && leaderRegex.test(request.text)) {
+        this.res.writeHead(200);
+        doLeaderboard();
+        this.res.end();
+    } else if (request.text && directionsRegex.test(request.text)) {
+        this.res.writeHead(200);
+        getDirections(request.text);
+        this.res.end();
+    } else {
+        console.log("don't care");
+        this.res.writeHead(200);
+        this.res.end();
+    }
 }
 
-function randomInt (low, high) {
-  return Math.floor(Math.random() * (high - low) + low);
+function randomInt(low, high) {
+    return Math.floor(Math.random() * (high - low) + low);
 }
 
 function postMessage() {
-  var botResponse, body, botReq;
+    let botResponse,
+        body,
+        botReq;
 
-  botResponse = cool();
+    botResponse = cool();
 
-  var number = randomInt(1,101);
-  if (number >= 90) {
-    botResponse = 'nerr';
-  }
-  else {
-    botResponse = 'ye';
-  }
-  sendResponse(botResponse);
+    const number = randomInt(1, 101);
+    if (number >= 90) {
+        botResponse = 'nerr';
+    } else {
+        botResponse = 'ye';
+    }
+    sendResponse(botResponse);
 }
 
 function sendResponse(botResponse) {
-  console.log('sending ' + botResponse + ' to ' + botID);
-  
-  var body = {
-    "bot_id" : botID,
-    "text" : botResponse
-  };
-  var botReq;
-  botReq = HTTPS.request(options, function(res) {
-    if(res.statusCode == 202) {
-      //neat
-    } else {
-      console.log('rejecting bad status code ' + res.statusCode);
-    }
-  });
+    console.log(`sending ${botResponse} to ${botID}`);
 
-  botReq.on('error', function(err) {
-    console.log('error posting message '  + JSON.stringify(err));
-  });
-  botReq.on('timeout', function(err) {
-    console.log('timeout posting message '  + JSON.stringify(err));
-  });
-  botReq.end(JSON.stringify(body));
+    const body = {
+        bot_id: botID,
+        text: botResponse
+    };
+    let botReq;
+    botReq = HTTPS.request(options, (res) => {
+        if (res.statusCode == 202) {
+            // neat
+        } else {
+            console.log(`rejecting bad status code ${res.statusCode}`);
+        }
+    });
 
-
+    botReq.on('error', (err) => {
+        console.log(`error posting message ${JSON.stringify(err)}`);
+    });
+    botReq.on('timeout', (err) => {
+        console.log(`timeout posting message ${JSON.stringify(err)}`);
+    });
+    botReq.end(JSON.stringify(body));
 }
 
 function sendEightBallMsg() {
-  sendResponse(predict());
+    sendResponse(predict());
 }
 
 function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
+    return Math.floor(Math.random() * Math.floor(max));
 }
 
 function tellJoke() {
-  const randomInt = getRandomInt(496);
-  if (randomInt < 86) {
-    const joke = _.shuffle(jokes)[0];
-    sendResponse(joke);
-  } else {
-    nodeRequest({url: 'https://icanhazdadjoke.com/', headers: {'Accept': 'application/json'}}, function (error, response, body) {
-      parsedData = JSON.parse(body);
-      if (parsedData.joke) {
-        sendResponse(parsedData.joke)
-      } else {
+    const randomInt = getRandomInt(496);
+    if (randomInt < 86) {
         const joke = _.shuffle(jokes)[0];
-        sendResponse(joke);    
-      }
-    });
-  }
+        sendResponse(joke);
+    } else {
+        nodeRequest({url: 'https://icanhazdadjoke.com/', headers: {Accept: 'application/json'}}, (error, response, body) => {
+            parsedData = JSON.parse(body);
+            if (parsedData.joke) {
+                sendResponse(parsedData.joke);
+            } else {
+                const joke = _.shuffle(jokes)[0];
+                sendResponse(joke);
+            }
+        });
+    }
 }
 
 
 function gifTag(message) {
-
-  nodeRequest('https://api.giphy.com/v1/gifs/search?q=' + message.split('#')[1].trim() + '&api_key=dc6zaTOxFJmzC&rating=r&limit=25', function (error, response, body) {
-    parsedData = JSON.parse(body);
-    console.log(`split msg: ${message.split('#')[1].trim()}`);
-    if (!error && response.statusCode == 200 && parsedData && parsedData.data) {
-      if (parsedData.data.length) {
-        var giphyResponse = _.shuffle(parsedData.data);
-        var botResponse = giphyResponse[0].images.downsized.url;
-        sendResponse(botResponse);    
-      }
-      else {
-        gifTag('#random')
-      }
-    } else {
-      console.log(message + ' is invalid');
-    }
-  });
+    nodeRequest(`https://api.giphy.com/v1/gifs/search?q=${message.split('#')[1].trim()}&api_key=dc6zaTOxFJmzC&rating=r&limit=25`, (error, response, body) => {
+        parsedData = JSON.parse(body);
+        console.log(`split msg: ${message.split('#')[1].trim()}`);
+        if (!error && response.statusCode == 200 && parsedData && parsedData.data) {
+            if (parsedData.data.length) {
+                const giphyResponse = _.shuffle(parsedData.data);
+                const botResponse = giphyResponse[0].images.downsized.url;
+                sendResponse(botResponse);
+            } else {
+                gifTag('#random');
+            }
+        } else {
+            console.log(`${message} is invalid`);
+        }
+    });
 }
 
 function doLeaderboard() {
-  var members = {};
-  var leaderboard = {};
-  var messages = [];
-  nodeRequest('https://api.groupme.com/v3/groups/21255858?token=' + apiKey, function (error, response, body) {
-    var parsedData = JSON.parse(body);
-    if (!error && response.statusCode == 200 && parsedData && parsedData.response) {
-      for (var i = 0; i < parsedData.response.members.length; i++) {
-        members[parsedData.response.members[i].user_id] = parsedData.response.members[i].nickname;
-        leaderboard[parsedData.response.members[i].nickname] = {likes: 0, likesGivenOut: 0};
-        leaderboard['GarrettBot'] = {likes: 0, likesGivenOut: 0};
-      }
-    }
-    nodeRequest('https://api.groupme.com/v3/groups/21255858/likes?period=month&token=' + apiKey, function (error, response, body) {
-      var parsedData = JSON.parse(body);
-      if (!error && response.statusCode == 200 && parsedData && parsedData.response) {
-        messages = parsedData.response.messages;
-        // console.log(parsedData.response);
-        
-      }
-      for (var j = 0; j < messages.length; j++) {
-        message = messages[j];
-        // console.log(message);
-        leaderboard[message.name].likes += message.favorited_by.length;
-        for (var i = 0; i < message.favorited_by.length; i++) {
-          leaderboard[members[message.favorited_by[i]]].likesGivenOut++;
+    const members = {};
+    const leaderboard = {};
+    let messages = [];
+    nodeRequest(`https://api.groupme.com/v3/groups/21255858?token=${apiKey}`, (error, response, body) => {
+        const parsedData = JSON.parse(body);
+        if (!error && response.statusCode == 200 && parsedData && parsedData.response) {
+            for (let i = 0; i < parsedData.response.members.length; i++) {
+                members[parsedData.response.members[i].user_id] = parsedData.response.members[i].nickname;
+                leaderboard[parsedData.response.members[i].nickname] = {likes: 0, likesGivenOut: 0};
+                leaderboard.GarrettBot = {likes: 0, likesGivenOut: 0};
+            }
         }
-    
-      }
+        nodeRequest(`https://api.groupme.com/v3/groups/21255858/likes?period=month&token=${apiKey}`, (error, response, body) => {
+            const parsedData = JSON.parse(body);
+            if (!error && response.statusCode == 200 && parsedData && parsedData.response) {
+                messages = parsedData.response.messages;
+                // console.log(parsedData.response);
+            }
+            for (let j = 0; j < messages.length; j++) {
+                message = messages[j];
+                // console.log(message);
+                leaderboard[message.name].likes += message.favorited_by.length;
+                for (let i = 0; i < message.favorited_by.length; i++) {
+                    leaderboard[members[message.favorited_by[i]]].likesGivenOut++;
+                }
+            }
 
-      console.log(leaderboard);
-      generateLeaderBoardResponse(leaderboard);
-  
-
+            console.log(leaderboard);
+            generateLeaderBoardResponse(leaderboard);
+        });
     });
-  
-
-  });
-  
 }
 function generateLeaderBoardResponse(leaderboard) {
-  var botResponse = 'Leaderboard:\n';
-  for (var i = 0; i < Object.keys(leaderboard).length; i++) {
-    botResponse += Object.keys(leaderboard)[i] + ':\n';
-    botResponse += `  Likes: ${leaderboard[Object.keys(leaderboard)[i]].likes}\n`;
-    botResponse += `  Likes given: ${leaderboard[Object.keys(leaderboard)[i]].likesGivenOut}\n`;
-  }
-  sendResponse(botResponse);
+    let botResponse = 'Leaderboard:\n';
+    for (let i = 0; i < Object.keys(leaderboard).length; i++) {
+        botResponse += `${Object.keys(leaderboard)[i]}:\n`;
+        botResponse += `  Likes: ${leaderboard[Object.keys(leaderboard)[i]].likes}\n`;
+        botResponse += `  Likes given: ${leaderboard[Object.keys(leaderboard)[i]].likesGivenOut}\n`;
+    }
+    sendResponse(botResponse);
 }
 
 function arrayToURLParam(locationArray) {
-  let urlParamString = '';
-  for (let i = 0; i < locationArray.length; i++) {
-    urlParamString += locationArray[i];
-    if (i < locationArray.length - 1) {
-      urlParamString += '+';
+    let urlParamString = '';
+    for (let i = 0; i < locationArray.length; i++) {
+        urlParamString += locationArray[i];
+        if (i < locationArray.length - 1) {
+            urlParamString += '+';
+        }
     }
-  }
-  return urlParamString;
+    return urlParamString;
 }
 
 function getDirections(directionString) {
-  var directionStringArray = directionString.split(' ');
-  directionStringArray.shift();
-  directionStringArray.shift();
-  var beginningArray = [];
-  var destinationArray = [];
-  while (directionStringArray[0].replace(':', '') !== 'to') {
-    beginningArray.push(directionStringArray.shift());
-  }
-  directionStringArray.shift();
-  destinationArray = directionStringArray;
-  const beginningLocString = arrayToURLParam(beginningArray);
-  const destLocString = arrayToURLParam(destinationArray);
-  const googleUrl = `https://maps.googleapis.com/maps/api/directions/json?origin=${beginningLocString}&destination=${destLocString}&key=${process.env.MAP_KEY}`;
-  nodeRequest(googleUrl, function (error, response, body) {
-	 
-    const jsonResponse = JSON.parse(body);
-    const urlShortenerUrl = `https://www.googleapis.com/urlshortener/v1/url?key=${process.env.URL_SHORT_KEY}`;
-    const googleMapsUri = `https://www.google.com/maps/dir/${beginningLocString}/${destLocString}`;
-    nodeRequest.post(urlShortenerUrl, {json: {longUrl: googleMapsUri}}, (response, body) => {
-      const shortUrl = body.body.id;
-      let botResponse =
+    const directionStringArray = directionString.split(' ');
+    directionStringArray.shift();
+    directionStringArray.shift();
+    const beginningArray = [];
+    let destinationArray = [];
+    while (directionStringArray[0].replace(':', '') !== 'to') {
+        beginningArray.push(directionStringArray.shift());
+    }
+    directionStringArray.shift();
+    destinationArray = directionStringArray;
+    const beginningLocString = arrayToURLParam(beginningArray);
+    const destLocString = arrayToURLParam(destinationArray);
+    const googleUrl = `https://maps.googleapis.com/maps/api/directions/json?origin=${beginningLocString}&destination=${destLocString}&key=${process.env.MAP_KEY}`;
+    nodeRequest(googleUrl, (error, response, body) => {
+        const jsonResponse = JSON.parse(body);
+        const urlShortenerUrl = `https://www.googleapis.com/urlshortener/v1/url?key=${process.env.URL_SHORT_KEY}`;
+        const googleMapsUri = `https://www.google.com/maps/dir/${beginningLocString}/${destLocString}`;
+        nodeRequest.post(urlShortenerUrl, {json: {longUrl: googleMapsUri}}, (response, body) => {
+            const shortUrl = body.body.id;
+            const botResponse =
       `Directions to ${destLocString.replace(/[+]/g, ' ')} from ${beginningLocString.replace(/[+]/g, ' ')}
 
 It will take ${jsonResponse.routes[0].legs[0].duration.text} to travel ${jsonResponse.routes[0].legs[0].distance.text}
 
-Click this to start navigation: ${shortUrl}`;    
-      sendResponse(botResponse);
-	  return;
+Click this to start navigation: ${shortUrl}`;
+            sendResponse(botResponse);
+        });
     });
-	return;
-  });
-  return;
 }
 
 exports.respond = respond;
--- index.js
+++ index.js
@@ -1,46 +1,49 @@
-var http, director, cool, bot, router, server, port;
+let http,
+    director,
+    cool,
+    bot,
+    router,
+    server,
+    port;
 
-http        = require('http');
-director    = require('director');
-cool        = require('cool-ascii-faces');
-bot         = require('./bot.js');
+http = require('http');
+director = require('director');
+cool = require('cool-ascii-faces');
+bot = require('./bot.js');
 
 
 router = new director.http.Router({
-  '/' : {
-    post: bot.respond,
-    get: ping
-  }
+    '/': {
+        post: bot.respond,
+        get: ping
+    }
 });
 
-server = http.createServer(function (req, res) {
-  req.chunks = [];
-  req.on('data', function (chunk) {
-    req.chunks.push(chunk.toString());
-  });
+server = http.createServer((req, res) => {
+    req.chunks = [];
+    req.on('data', (chunk) => {
+        req.chunks.push(chunk.toString());
+    });
 
-  router.dispatch(req, res, function(err) {
-    res.writeHead(err.status, {"Content-Type": "text/plain"});
-    res.end(err.message);
-  });
+    router.dispatch(req, res, (err) => {
+        res.writeHead(err.status, {'Content-Type': 'text/plain'});
+        res.end(err.message);
+    });
 });
 
 port = Number(process.env.PORT || 5000);
 server.listen(port);
 
 function ping() {
-  var ye = '@garrettbot #jay cutler'
-  var regex = /@garrettbot #[a-zA-Z ]+/
-  var retval = '';
-  if (regex.test(ye)) {
-    retval = ye.substring(13).trim();
-    this.res.writeHead(200);
-    this.res.end('ye');
-  
-  }
-  else {
-    this.res.writeHead(200);
-    this.res.end('ye');
-  
-  }
+    const ye = '@garrettbot #jay cutler';
+    const regex = /@garrettbot #[a-zA-Z ]+/;
+    let retval = '';
+    if (regex.test(ye)) {
+        retval = ye.substring(13).trim();
+        this.res.writeHead(200);
+        this.res.end('ye');
+    } else {
+        this.res.writeHead(200);
+        this.res.end('ye');
+    }
 }
